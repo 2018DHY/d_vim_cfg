af38b3c9a45a38f4a14a76fb643a8d18804a5ca4
DHY <42273203+2018DHY@users.noreply.github.com>
2026-04-10 13:56:38 +0800

    Change shadowsocks-libev installation from snap to apt

--- pp_system.sh
+++ pp_system.sh
@@ -2,6 +2,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install build-essential
 sudo apt install git vim cmake neofetch ssh privoxy
-sudo snap install shadowsocks-libev
+sudo apt install shadowsocks-libev
 #for qt
 sudo apt install libxcb-cursor0 libxcb-cursor-dev
