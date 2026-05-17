56eb84b66756f976662ea303fb0d19d9b9efa1f6
DHY <42273203+2018DHY@users.noreply.github.com>
2026-05-17 10:07:46 +0800

    Update package installation in pp_system.sh
    Removed 'neofetch' from the installation list.

--- pp_system.sh
+++ pp_system.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install build-essential -y
-sudo apt install git vim cmake neofetch ssh privoxy -y
+sudo apt install git vim cmake ssh privoxy -y
 sudo apt install shadowsocks-libev -y
 #for qt
 sudo apt install libxcb-cursor0 libxcb-cursor-dev -y
