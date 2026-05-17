c1d6118e434199abd0c68193204c68d3d7f1f022
DHY <42273203+2018DHY@users.noreply.github.com>
2026-05-17 09:53:42 +0800

    Refactor package installation in pp_system.sh
    Removed emacs from the initial installation list and added it later along with curl.

--- pp_system.sh
+++ pp_system.sh
@@ -1,7 +1,9 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install build-essential -y
-sudo apt install git vim cmake neofetch ssh privoxy emacs -y
+sudo apt install git vim cmake neofetch ssh privoxy -y
 sudo apt install shadowsocks-libev -y
 #for qt
 sudo apt install libxcb-cursor0 libxcb-cursor-dev -y
+sudo apt install emacs -y
+sudo apt install curl -y
