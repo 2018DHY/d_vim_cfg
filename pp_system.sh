6d87ba6e9c1e47f53b2769ae4cc9b85c97d929a6
DHY <42273203+2018DHY@users.noreply.github.com>
2026-05-17 09:46:54 +0800

    Add -y flag to package installations in pp_system.sh

--- pp_system.sh
+++ pp_system.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install build-essential
-sudo apt install git vim cmake neofetch ssh privoxy
-sudo apt install shadowsocks-libev
+sudo apt install git vim cmake neofetch ssh privoxy emacs -y
+sudo apt install shadowsocks-libev -y
 #for qt
-sudo apt install libxcb-cursor0 libxcb-cursor-dev
+sudo apt install libxcb-cursor0 libxcb-cursor-dev -y
