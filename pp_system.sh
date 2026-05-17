ce9a22cd4ac7c103e701c4bfa4464dc12ae90e16
DHY <42273203+2018DHY@users.noreply.github.com>
2026-05-17 09:47:14 +0800

    Add -y flag to build-essential installation

--- pp_system.sh
+++ pp_system.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install build-essential
+sudo apt install build-essential -y
 sudo apt install git vim cmake neofetch ssh privoxy emacs -y
 sudo apt install shadowsocks-libev -y
 #for qt
