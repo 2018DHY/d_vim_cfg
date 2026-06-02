6f339190cdaf8d11bbe1835b9163c4192b42d54c
DHY <42273203+2018DHY@users.noreply.github.com>
2026-06-02 09:19:59 +0800

    Add pkg-config to installation command in pp_system.sh

--- pp_system.sh
+++ pp_system.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt install build-essential gdb libgl1-mesa-dev -y
+sudo apt install build-essential gdb pkg-config libgl1-mesa-dev -y
 sudo apt install git vim cmake ssh privoxy fastfetch -y
 sudo apt install shadowsocks-libev -y
 #for qt
